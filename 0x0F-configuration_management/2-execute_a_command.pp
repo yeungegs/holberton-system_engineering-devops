854127ab7727f857072cc8dfb724096834dbe908
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-08-18 17:56:13 -0700

    Add Task 2 - v1

--- 0x0F-configuration_management/2-execute_a_command.pp
+++ 0x0F-configuration_management/2-execute_a_command.pp
@@ -1,1 +1,5 @@
-#!/usr/bin/env bash
+# Using Puppet, create a manifest that kills a process named killmenow
+exec { 'killmenow':
+  command => 'pkill -f killmenow'
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
+}
