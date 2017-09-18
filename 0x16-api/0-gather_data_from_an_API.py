d1d02341988f5c62eb4cc8ded6ea9ea7c393a218
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-09-18 16:57:50 -0700

    Add Task 0 - v1

--- 0x16-api/0-gather_data_from_an_API.py
+++ 0x16-api/0-gather_data_from_an_API.py
@@ -8,16 +8,16 @@ if __name__ == "__main__":
     employeeid = sys.argv[1]
     EMPLOYEE_NAME = requests.get("https://jsonplaceholder.typicode.com/users/{:d}"
                         .format(int(employeeid))).json().get("name")
-    tasksall = 0
-    taskscompleted = []
+    DONE_TASKS = []
+    TOTAL_NUMBER_OF_TASKS = 0
     r = requests.get("https://jsonplaceholder.typicode.com/todos").json()
 
     for task in r:
         if (task.get("userId") == int(employeeid)):
-            tasksall += 1
+            TOTAL_NUMBER_OF_TASKS += 1
             if (task.get("completed")):
-                taskscompleted.append(task.get("title"))
+                DONE_TASKS.append(task.get("title"))
     print("Employee {:s} is done with tasks({:d}/{:d}):".format
-          (EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-    for task in taskscompleted:
+          (EMPLOYEE_NAME, len(DONE_TASKS), TOTAL_NUMBER_OF_TASKS))
+    for task in DONE_TASKS:
         print("\t {:s}".format(task))
