8978b7aa29aa8f2dffd08d9bf2a798055d742de5
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-09-18 20:55:12 -0700

    Update Task 0 - update variable names to match consistent formatting

--- 0x16-api/0-gather_data_from_an_API.py
+++ 0x16-api/0-gather_data_from_an_API.py
@@ -5,20 +5,22 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    employeeid = sys.argv[1]
+    EMPLOYEE_ID = sys.argv[1]
     EMPLOYEE_NAME = requests.get(
         "https://jsonplaceholder.typicode.com/users/{:d}"
-        .format(int(employeeid))).json().get("name")
+        .format(int(EMPLOYEE_ID))).json().get("name")
     DONE_TASKS = []
     TOTAL_NUMBER_OF_TASKS = 0
     r = requests.get("https://jsonplaceholder.typicode.com/todos").json()
 
     for task in r:
-        if (task.get("userId") == int(employeeid)):
+        if (task.get("userId") == int(EMPLOYEE_ID)):
             TOTAL_NUMBER_OF_TASKS += 1
             if (task.get("completed")):
                 DONE_TASKS.append(task.get("title"))
+
     print("Employee {:s} is done with tasks({:d}/{:d}):".format
           (EMPLOYEE_NAME, len(DONE_TASKS), TOTAL_NUMBER_OF_TASKS))
+
     for task in DONE_TASKS:
         print("\t {:s}".format(task))
