623f901cbd1a258ce8ac56894a05037b47ad21dd
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-09-18 20:27:26 -0700

    Add Task 3 - v1

---
+++ 0x16-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Python script that extends script from Task 0
+and exports data in JSON formatted file
+File must have all records from all employees
+"""
+import json
+import requests
+
+if __name__ == "__main__":
+    ALL_USERS = requests.get(
+        "https://jsonplaceholder.typicode.com/users").json()
+    ALL_TASKS = requests.get(
+        "https://jsonplaceholder.typicode.com/todos").json()
+    ALL_RECORDS = {}
+
+    for user in ALL_USERS:
+        EMPLOYEE_ID = user.get("id")
+        print("id: {}".format(EMPLOYEE_ID))
+        USERNAME = user.get("username")
+        print("username: {}".format(USERNAME))
+
+        for task in ALL_TASKS:
+            if (task.get("userId") == int(EMPLOYEE_ID)):
+                dict = {}
+                dict["task"] = task.get("title")
+                dict["completed"] = task.get("completed")
+                dict["USERNAMEOC"] = USERNAME
+                ALL_TASKS.append(dict)
+
+        ALL_RECORDS[EMPLOYEE_ID] = ALL_TASKS
+
+    with open("todo_all_employees.json", 'w') as jsonfile:
+        json.dump(ALL_RECORDS, jsonfile)
