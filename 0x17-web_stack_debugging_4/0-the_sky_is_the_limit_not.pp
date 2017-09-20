346fb42128405843f060da18541651a4927a875a
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-09-19 23:42:27 -0700

    Update Task 0 - fix quotation mark

--- 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
 # puppet script to debug web server failed requests
 exec { 'increase limit on open files':
-  command => '/bin/sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"- Hn\"/" /etc/default/nginx && /etc/init.d/nginx restart",
+  command => '/bin/sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"- Hn\"/" /etc/default/nginx && /etc/init.d/nginx restart',
   }
