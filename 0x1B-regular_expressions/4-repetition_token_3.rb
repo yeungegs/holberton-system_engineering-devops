853d0ba907291ef8f651984a684ab6beb9d511a4
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-10-12 03:02:34 +0000

    Tasks 2-5 - v1 :tada:

--- 0x1B-regular_expressions/4-repetition_token_3.rb
+++ 0x1B-regular_expressions/4-repetition_token_3.rb
@@ -1,1 +1,3 @@
 #!/usr/bin/env ruby
+# Ruby script that accepts one argument and pass it to a regular expression matching method
+puts ARGV[0].scan(/hbt*n/).join
--- 0x1B-regular_expressions/5-beginning_and_end.rb
+++ 0x1B-regular_expressions/5-beginning_and_end.rb
@@ -1,1 +1,4 @@
 #!/usr/bin/env ruby
+# Ruby script that accepts one argument and pass it to a regular expression matching method
+# regex must be exactly matchin a string that starts by h ends by n and can have a single character in between
+puts ARGV[0].scan(/h.n/).join
