7e8c7b737f38b087a600ebd524aa3a70ca30a2a0
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-10-12 07:08:01 +0000

    Task 100 - work in progress

--- 0x1B-regular_expressions/100-textme.rb
+++ 0x1B-regular_expressions/100-textme.rb
@@ -1,1 +1,8 @@
 #!/usr/bin/env ruby
+# 
+#puts ARGV[0].scan(/\[([^S|A|B|F||u\]]+?)\]/).join(", ").scan(/<([^>]*)>/).last.first
+str = ARGV[0].scan(/\[([^\]([TFSu])]*)\]/).join(", ")
+puts str
+str.split(/\w+:(.*), to:(.*), flags:(.*)/)
+puts str
+
