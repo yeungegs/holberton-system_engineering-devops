b302d51f979da0517e5417679fa468303afb82ac
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-10-12 03:13:30 +0000

    Add Task 7 - v1 :tada:

--- 0x1B-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x1B-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,1 +1,5 @@
 #!/usr/bin/env ruby
+# Ruby script that accepts one argument and pass it to a regular expression matching method
+# The regular expression must only match capital letters
+puts ARGV[0].scan(/[A-Z]/).join
+
