3e9e9bca2c99fcd9770c8ac97df3c5d1b02425a6
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-10-12 20:03:18 +0000

    Task 6 - update script to include end of line

--- 0x1B-regular_expressions/6-phone_number.rb
+++ 0x1B-regular_expressions/6-phone_number.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # Ruby script that accepts one argument and pass it to a regular expression matching method
 # The regular expression must match a 10 digit phone number
-puts ARGV[0].scan(/^\d{10}/).join
+puts ARGV[0].scan(/^\d{10}$/).join
